af2eb2a58fef414dbc21da14f48213ad50376deb
Halis Duraki <duraki.halis@nsoft.ba>
2017-09-10 23:43:51 +0200

    init helper / version

---
+++ lib/devist/helper.rb
@@ -0,0 +1,7 @@
+# Base Devist helper class. Provides small reusable snippet or methods that
+# will increase productivity.
+#
+# @author Halis Duraki
+class Devist::Helper
+
+end
---
+++ lib/devist/version.rb
@@ -0,0 +1,5 @@
+class Devist
+
+  VERSION = "1.1.4".freeze
+
+end
