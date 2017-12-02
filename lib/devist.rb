0ac43662ce897150955d2a6f128cfbc90f2a583a
Halis Duraki <duraki@null.net>
2017-12-02 13:31:26 +0100

    fix: refactor devist ep

--- lib/devist.rb
+++ lib/devist.rb
@@ -2,85 +2,45 @@
 # value for compiler.
 #
 # @author Halis Duraki
-# @!attribute [r] count
 #     @return parser [Object] module for parsing data init 
 #     @return compiler [Object] module for compiling data init
 class Devist
 
   attr_reader :parser, :compiler
+  attr_reader :logfile, :theme
 
   # Register default files to search for.
   @@default_search = %w[CHANGELOG RELEASES NEWS]
 
   # Initialize Devist command line interface. 
   #
-  # @param [String] changelog file
-  # @param [String] export/html theme style 
-  # @return self
+  # @param [String] filename
+  # @param [String] theme 
+  # @return [void]
   def initialize(filename, theme = 'default')
-  
     @parser = Parser.new
-    @filename = filename + ".md"
-    @themename = "_" + theme + ".html.erb"
 
-    # Theme directory is absolute dirpath + subs
-    theme_dir = __dir__ + "/devist/export/html/"
-
-    # Check if both theme and filename exists
-    if File.file?(@filename) 
-      print "  * File '#{@filename}' exists; continuing ...\n"
-    else
-      print "  * File '#{@filename}' does NOT exists; exit ...\n"
-      print "  > Do you want to allow devist to search the file for you? (y\/N) "
-      devist_as = STDIN.gets.chomp
-      
-      # Search for changelogs automatically
-      if devist_as.downcase.eql?("y")
-          print "  * Searching for changelog data ...\n"
-          available
-
-          if @@available.count > 0
-            print "  * Try to run with filename #{@@available_list}\n"
-          end
-      end
-          
-      exit
-    end
-
-    if File.file?(theme_dir + @themename)
-      print "  * Theme '#{@themename}' exists; continuing ...\n"
-    else
-      print "  * Theme '#{@themename}' does NOT exists; exit ...\n"
-      exit
-    end
-
+    is_changelog?("#{filename}.md")
+    is_theme?("#{theme}")
   end
 
-  # Autosearch default & available changelog in working directory.
+  # Autosearch default & get first available logfiles in a working directory.
   #
-  # @return [Integer] found logfile 
+  # @return [String] logfile 
   def available
-    @@available = []
-    @@available_list = String.new 
-
     @@default_search.each do |filename|
-      filename.concat(".md")
-      if File.file?(filename)
-        @@available_list.concat(filename + " ")
-        @@available.push(filename)   
+      if File.file?("#{filename}.md")
+        print "  * Found possible logfile `#{filename.downcase}.md` in directory.\n"
+        return filename
       end
     end
-
-    print "  * Found #{@@available.count} results.\n"
-
-    @@available.count
   end
 
   # Decompile markdown file to raw data.
   #
   # @return [Object] project data
   def decompile
-    @parser.parse_data(@filename)
+    @parser.parse_data(@logfile)
     @parser.project
   end
 
@@ -88,8 +48,62 @@ class Devist
   #
   # @return [Object] compiler 
   def recompile
-    @compiler = Compiler.new(@parser.project, @parser.changelog, @themename)
+    @compiler = Compiler.new(@parser.project, @parser.changelog, @theme)
     @compiler.compile_data
+  end
+
+  # Get theme directory, an absolute devist gem path plus theme prefix.
+  #
+  # @return [String] theme directory
+  def theme_dir
+    "#{__dir__}/devist/export/html"
+  end
+
+  # Build theme and add sufix to absolute theme directory and filename.
+  #
+  # @param [String] name
+  # @return [String] absolute theme file
+  def theme_name(name)
+    "#{theme_dir}/_#{name}.html.erb"
+  end
+
+  # Check if given argument is changelog file. Search for additional changelogs
+  # if non is found.
+  #
+  # @param [String] filename
+  # @return [String] logfile
+  def is_changelog?(filename)
+    if !File.file?(filename)
+      print "  * Changelog '#{@filename}' does NOT exists; exit ...\n"
+      print "  > Do you want to allow devist to search the file for you? #{@@default_search} (y\/N) ".green
+      devist_as = STDIN.gets.chomp
+
+      exit if !devist_as.downcase.eql?("y")
+
+      print "  * Searching for changelog data ...\n"
+
+      if logfile = available
+        print "  * Try to run with command: `devist #{logfile}`\n".blue
+        exit
+      end
+    end
+
+    @logfile = filename
+  end
+
+  # Check if given argument is theme file. Search for theme and prepare for ERB
+  # ops.
+  #
+  # @param [String] filename
+  # @return [String] themefile
+  def is_theme?(filename)
+    if File.file?(theme_name(filename))
+      print "  * Theme '#{theme_name(filename)}' exists; continuing ...\n"
+      @theme = theme_name(filename)
+    else
+      print "  * Theme '#{theme_name(filename)}' does NOT exists; exit ...\n"
+      exit
+    end
   end
 
 end
