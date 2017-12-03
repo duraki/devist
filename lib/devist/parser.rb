f7aa73fcdba96d6675924318a0c45b368cf90927
Halis Duraki <duraki@null.net>
2017-12-03 17:40:05 +0100

    fix: fix strip; add new tags; parse gnu/kac

--- lib/devist/parser.rb
+++ lib/devist/parser.rb
@@ -5,28 +5,38 @@ require 'date'
 # structure.
 #
 # @author Halis Duraki
-# @!attribute [r] count
-#     @return project [Object] project model data
-#     @return changelog [Object] changelog model data
 class Devist::Parser
 
-  attr_reader :project, :changelog
+  # @return [Object] Project instance
+  attr_reader :project
 
-  # Build and extract project details by front-matter.
+  # @return [Object] Changelog instance
+  attr_reader :changelog
+
+  # @return [Object] Change indexes
+  attr_reader :indexes 
+
+  TAGS = ["ADDED", "CHANGED", "FIXED", "REMOVED", "DEPRECATED", "SECURITY"]
+
+  def initialize
+    @indexes = {}
+  end
+
+  # Build and extract project details by fmm.
   #
   # @param [String] line to cmp to
   # @return [String] project detail
   def build_info(line)
     case line
     when /@project:+/
-      @project.name = Devist::Extractor.extract_info(line)
-      print "  * Extracting project name       ...   [#{@project.name.chomp.strip!}]\n"
+      @project.name = Devist::Extractor.extract_info(line) 
+      print "  * Extracting project name       ...   [#{@project.name.strip!}]\n"
     when /@author:.+/
       @project.author = Devist::Extractor.extract_info(line)
-      print "  * Extracting project author     ...   [#{@project.author.chomp.strip!}]\n"
+      print "  * Extracting project author     ...   [#{@project.author.strip!}]\n"
     when /@homepage:.+/
       @project.homepage = Devist::Extractor.extract_info(line)
-      print "  * Extracting project homepage   ...   [#{@project.homepage.chomp.strip!}]\n"
+      print "  * Extracting project homepage   ...   [#{@project.homepage.strip!}]\n"
     end
   end
 
@@ -34,9 +44,57 @@ class Devist::Parser
   #
   # @param [String] line to cmp to
   # @return [Object] built changelog
-  def build_changelog(line)
+  def build_changelog(line, index)
     build_version(line)
     build_tags(line)
+    build_tags_kac(line, index)
+  end
+
+  def read_change_of_index(i, type)
+    i += 1 # => Always add index to /###/ match
+    f = File.readlines(@filename).to_a
+
+    while i.to_i < f.count.to_i
+      return if f.at(i).to_s.match(/###.+/) # => Return match on next known tag
+      next if f.at(i).to_s.empty? # => Return match on empty change
+      @changelog[@version].tag type, Devist::Extractor.extract_change_kac(f.at(i.to_i))
+      i+=1
+    end
+  end
+
+  # Build changelog tags and changes index based on gnu/kac format.
+  #
+  # @param [String] line to cmp to
+  # @param [Integer] line index
+  # @return [Void] 
+  def build_tags_kac(line, i)
+    case line
+    when /### Added/, /### Add/
+      @indexes.store("added", i)
+      read_change_of_index(@indexes["added"], 'added')
+    when /### Changed/, /### Change/
+      @indexes.store("changed", i)
+      read_change_of_index(@indexes["changed"], 'changed')
+    when /### Fixed/, /### Fix/
+      @indexes.store("fixed", i)
+      read_change_of_index(@indexes["fixed"], 'fixed')
+    when /### Removed/, /### Remove/
+      @indexes.store("removed", i)
+      read_change_of_index(@indexes["removed"], 'removed')
+    when /### Deprecated/, /### Deprecate/
+      @indexes.store("deprecated", i)
+      read_change_of_index(@indexes["deprecated"], 'deprecated')
+    when /### Security/
+      @indexes.store("security", i)
+      read_change_of_index(@indexes["security"], 'security')
+    end
+
+    puts @indexes.sort_by {|_key, v| v}.to_h
+  end
+
+  def read_changes_from_to(i_start, i_stop)
+    puts @filename 
+    exit
   end
 
   # Build changelog tags and changes.
@@ -45,14 +103,18 @@ class Devist::Parser
   # @return [Object] changelog tag
   def build_tags(line)
     case line
-    when /#added.+/
+    when /#added.+/ # => todo: /### Added/
+      @changelog[@version].tag 'added', Devist::Extractor.extract_change(line)
+    when /#changed.+/ 
       @changelog[@version].tag 'added', Devist::Extractor.extract_change(line)
     when /#fixed.+/
       @changelog[@version].tag 'fixed', Devist::Extractor.extract_change(line)
     when /#removed.+/
       @changelog[@version].tag 'removed', Devist::Extractor.extract_change(line)
-    when /#improved.+/
-      @changelog[@version].tag 'improved', Devist::Extractor.extract_change(line)
+    when /#deprecated.+/
+      @changelog[@version].tag 'deprecated', Devist::Extractor.extract_change(line)
+    when /#security.+/
+      @changelog[@version].tag 'security', Devist::Extractor.extract_change(line)
     end
   end
 
@@ -62,6 +124,11 @@ class Devist::Parser
   # @return [Object] changelog version
   def build_version(line)
     case line
+      when /\[[0-9]+.[0-9]+.[0-9]+]/ # => improved from: \[*.+]
+      @date = Date.parse(line)
+      @version += 1
+      @changelog[@version] = Devist::Version.new (Devist::Extractor.extract_version_kac line), @date
+
       when /### Version+/
       @date = Date.parse(line) # Extract version date
       @version += 1 # Increment version
@@ -99,15 +166,26 @@ class Devist::Parser
     @project = Devist::Project.new
     @changelog = []
     @version = -1 # Start from 0
+    @filename = file_name
 
     devist?(file_name) # Check if file is configured for usage
 
     print "  * Building model from file data ...\n"
 
-    File.foreach(file_name) do |line|
+    File.foreach(file_name).each_with_index do |line, i|
       build_info(line) # Build project info
-      build_changelog(line) # Build changelog data
+      build_changelog(line, i) # Build changelog data
     end
+
+    #puts @changelog[@version].inspect
+    #l = File.open(@filename).to_a
+    #puts l.at(@indexes[:added])
+    #exit
+
+    #@indexes.each_with_index do |t, j|
+      #puts t,j
+    #end
+    #read_changes_from_to
 
     @changelog
   end
